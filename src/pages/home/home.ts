bc2ba1dd66cca93f874487865e401956977e5773
ImClaudiaF-PC\ImClaudiaF <fclaudia.93@gmail.com>
2017-11-12 11:54:30 -0400

    Primera app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,31 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  name: string;
+  test:number[];
+  flag:boolean;
+  numbers:number[];
+  addNumber:number;
   constructor(public navCtrl: NavController) {
+    this.name='Claudia';
+    this.test=[1,2,5,3];
+    this.flag=false;
+    this.numbers=[1,2];
+  }
 
+  // ionViewDidLoad(){
+  //   alert('welcome')
+  // }
+
+  
+
+  click(){
+    alert(this.name);
+  }
+
+  add(num){
+    this.numbers.push(num);
+    console.log(num)
   }
 
 }
